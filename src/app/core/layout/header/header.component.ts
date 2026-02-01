cf9b32f469258dcdbdbdc56f911f90cd25a8e451
Nicolás Butterfield <nicobutter@gmail.com>
2026-02-01 19:42:33 -0300

    feat(header): enhance header component with scroll effect and social media links

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isShrunk = false;
+
+  @HostListener('window:scroll')
+  onWindowScroll() {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    this.isShrunk = scrollTop > 50;
+  }
+}
